f5f30a8a04824dc32f879cc7c616c7672c7acf13
Tobias Gutmann <tobias.gutmann@methodpark.de>
2019-11-01 13:45:19 +0100

    Enable importing json files as typed objects
    The tsconfig.json needs a new entry: `resolveJsonModule`
    Adding this property means, that any json import will
    now have type support.

--- src/about.ts
+++ src/about.ts
@@ -1,16 +1,23 @@
 import chalk from 'chalk';
 import figlet from 'figlet';
-import { emptyLine, printSorroundedWithDashes } from './formatSnippets';
 
-export const about = () => {
-	console.log(
-		chalk.red(
-			figlet.textSync('glab', '3D Diagonal')
-		)
-    )
+import * as gitlabExampleIssueResponse from '../api-responses/gitlabExampleIssueResponse.json';
+import {emptyLine, printSorroundedWithDashes} from './formatSnippets';
+import { Issues } from '../models/issues.js';
 
-    displayContributersWanted();
-}
+const gitlabIssueExample = gitlabExampleIssueResponse as unknown as Issues;
+
+export function about() {
+  console.log(chalk.red(figlet.textSync('glab', '3D Diagonal')));
+  displayContributersWanted();
+	emptyLine();
+
+	console.log("Glab helps you manage your gitlab issues.")
+	emptyLine();
+	console.log('Issue example response from gitlab:')
+	console.log(gitlabIssueExample);
+
+};
 
 const displayContributersWanted = () => {
     emptyLine();
